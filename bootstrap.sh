e67f58e0da395eb57039054d350b727126735bfa
Sergunik <sergunik@serguniks-mbp.kyiv.epam.com>
2015-07-24 15:45:19 +0300

    init commit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-get update 2> /dev/null
+
+sudo apt-get install -y mc 2> /dev/null
+sudo apt-get install -y git 2> /dev/null
+sudo apt-get install -y nginx 2> /dev/null
+
+sudo apt-get install -y php5 php5-fpm php-pear php5-dev php5-gd php5-curl php5-mcrypt php5-cli 2> /dev/null
